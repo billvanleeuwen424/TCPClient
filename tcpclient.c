df25ba15324390f5ef92dcb31394a20ad32f90f8
billvanleeuwen424 <billvanleeuwen424@gmail.com>
2022-04-25 16:35:48 -0400

    socket connection

--- tcpclient.c
+++ tcpclient.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
     memset(&hints, 0, sizeof(hints));   //zero out hints
     hints.ai_socktype = SOCK_STREAM;
 
+
     struct addrinfo *peer_address;
 
     //get address info, store in peer_address
@@ -20,23 +21,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+
     //print address info to console
     char addressBuff[25], serviceBuff[25];
     getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen, addressBuff, sizeof(addressBuff), serviceBuff, sizeof(serviceBuff), NI_NUMERICHOST);
     printf("Connection successful. Address is: %s %s\n", addressBuff, serviceBuff);
 
+
+    //create socket and error check
     int socket_peer;
     if((socket_peer = socket(peer_address->ai_family, peer_address->ai_socktype, peer_address->ai_protocol)) < 0){
         fprintf(stderr, "Socket creation failure: %s\n", strerror(errno));
         return 1;
     }
 
+    //connect to server and error check
+    if(connect(socket_peer, peer_address->ai_addr, peer_address->ai_addrlen)){
+        fprintf(stderr, "Connection failure: %s\n", strerror(errno));
+        return 1;
+    }
+
     
-
-
-
-
-
-
 
 }
